e76fd67a9ced32c100e0d82bf068279fec34f8f3
Hailey <haileystrobelt@gmail.com>
2021-04-06 23:51:56 -0600

    Fog/Texture Animation

---
+++ CTR Environment/Assets/Misc/ScrollTexture.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollTexture : MonoBehaviour
+{
+    public float ScrollX = 0.5f;
+    public float ScrollY = 0.5f;
+    private Renderer m_Renderer;
+
+    private void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float OffsetX = Time.time * ScrollX;
+        float OffsetY = Time.time * ScrollY;
+        m_Renderer.material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+    }
+}
